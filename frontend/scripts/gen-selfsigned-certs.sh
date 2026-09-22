b2554102abd27f899f17e7b8a5201f9647985093
NF-coder <resetnikovs72@gmail.com>
2026-09-22 23:22:19 +0300

    refactor(gen-selfsigned-certs): now selfsigned sertificates stored in `frontend/certs` instead of `frontend/nginx-conf/certs`

--- frontend/scripts/gen-selfsigned-certs.sh
+++ frontend/scripts/gen-selfsigned-certs.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-mkdir -p nginx-conf/certs
+mkdir -p certs
 
-if [ ! -f nginx-conf/certs/selfsigned.crt ] || [ ! -f nginx-conf/certs/selfsigned.key ]; then
+if [ ! -f certs/selfsigned.crt ] || [ ! -f certs/selfsigned.key ]; then
     echo "Сертификаты не найдены. Генерируем новые."
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-      -keyout nginx-conf/certs/selfsigned.key \
-      -out nginx-conf/certs/selfsigned.crt \
+      -keyout certs/selfsigned.key \
+      -out certs/selfsigned.crt \
       -subj "/C=RU/ST=Moscow/L=Moscow/O=Dev/OU=IT/CN=localhost"
 else
     echo "Сертификаты уже существуют. Пропускаем генерацию."
